cb1997435a14d044a4cd2b8c86f8701439cf53d3
julian hurtado <desarrollador.ti3@gmail.com>
2023-12-12 16:58:07 -0500

    cambios

--- database/seeders/ImgThemesWorldSeeder.php
+++ database/seeders/ImgThemesWorldSeeder.php
@@ -16,92 +16,92 @@ class ImgThemesWorldSeeder extends Seeder
         DB::table('img_themes_world')->insert(
             array(
                 array(
-                    'img_name' => '',
-                    'img_theme_world' => '',
+                    'img_name' => 'the_ancient_hallows.jpg',
+                    'img_theme_world' => 'the_ancient_hallows.jpg',
                     'theme_world_title' => 'The Ancient Hallows',
                     'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, laudantium voluptatum perferendis assumenda totam amet, praesentium qui nihil non aperiam excepturi error maxime ab dolore, accusantium eveniet explicabo nostrum eaque?'
                 ),
                 array(
-                    'img_name' => '',
-                    'img_theme_world' => '',
+                    'img_name' => 'the_bridges.jpg',
+                    'img_theme_world' => 'the_bridges.jpg',
                     'theme_world_title' => 'The Bridges',
                     'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, laudantium voluptatum perferendis assumenda totam amet, praesentium qui nihil non aperiam excepturi error maxime ab dolore, accusantium eveniet explicabo nostrum eaque?'
                 ),
                 array(
-                    'img_name' => '',
-                    'img_theme_world' => '',
+                    'img_name' => 'the_clans.jpg',
+                    'img_theme_world' => 'the_clans.jpg',
                     'theme_world_title' => 'The Clans',
                     'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, laudantium voluptatum perferendis assumenda totam amet, praesentium qui nihil non aperiam excepturi error maxime ab dolore, accusantium eveniet explicabo nostrum eaque?'
                 ),
                 array(
-                    'img_name' => '',
-                    'img_theme_world' => '',
+                    'img_name' => 'the_conflict.jpg',
+                    'img_theme_world' => 'the_conflict.jpg',
                     'theme_world_title' => 'The Conflict',
                     'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, laudantium voluptatum perferendis assumenda totam amet, praesentium qui nihil non aperiam excepturi error maxime ab dolore, accusantium eveniet explicabo nostrum eaque?'
                 ),
                 array(
-                    'img_name' => '',
-                    'img_theme_world' => '',
+                    'img_name' => 'the_continents.jpg',
+                    'img_theme_world' => 'the_continents.jpg',
                     'theme_world_title' => 'The Continents',
                     'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, laudantium voluptatum perferendis assumenda totam amet, praesentium qui nihil non aperiam excepturi error maxime ab dolore, accusantium eveniet explicabo nostrum eaque?'
                 ),
                 array(
-                    'img_name' => '',
-                    'img_theme_world' => '',
+                    'img_name' => 'the_culture.jpg',
+                    'img_theme_world' => 'the_culture.jpg',
                     'theme_world_title' => 'The Culture',
                     'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, laudantium voluptatum perferendis assumenda totam amet, praesentium qui nihil non aperiam excepturi error maxime ab dolore, accusantium eveniet explicabo nostrum eaque?'
                 ),
                 array(
-                    'img_name' => '',
-                    'img_theme_world' => '',
+                    'img_name' => 'the_currency_system.jpg',
+                    'img_theme_world' => 'the_currency_system.jpg',
                     'theme_world_title' => 'The Currency System',
                     'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, laudantium voluptatum perferendis assumenda totam amet, praesentium qui nihil non aperiam excepturi error maxime ab dolore, accusantium eveniet explicabo nostrum eaque?'
                 ),
                 array(
-                    'img_name' => '',
-                    'img_theme_world' => '',
+                    'img_name' => 'the_eternal_storm_in_the_sea.jpg',
+                    'img_theme_world' => 'the_eternal_storm_in_the_sea.jpg',
                     'theme_world_title' => 'The Eternal Storm in the Sea',
                     'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, laudantium voluptatum perferendis assumenda totam amet, praesentium qui nihil non aperiam excepturi error maxime ab dolore, accusantium eveniet explicabo nostrum eaque?'
                 ),
                 array(
-                    'img_name' => '',
-                    'img_theme_world' => '',
+                    'img_name' => 'the_lord_of_the_grave.jpg',
+                    'img_theme_world' => 'the_lord_of_the_grave.jpg',
                     'theme_world_title' => 'The Lord of the Grave',
                     'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, laudantium voluptatum perferendis assumenda totam amet, praesentium qui nihil non aperiam excepturi error maxime ab dolore, accusantium eveniet explicabo nostrum eaque?'
                 ),
                 array(
-                    'img_name' => '',
-                    'img_theme_world' => '',
+                    'img_name' => 'the_curse_of_merak.jpg',
+                    'img_theme_world' => 'the_curse_of_merak.jpg',
                     'theme_world_title' => 'The Curse of Merak',
                     'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, laudantium voluptatum perferendis assumenda totam amet, praesentium qui nihil non aperiam excepturi error maxime ab dolore, accusantium eveniet explicabo nostrum eaque?'
                 ),
                 array(
-                    'img_name' => '',
-                    'img_theme_world' => '',
+                    'img_name' => 'the_naventi.jpg',
+                    'img_theme_world' => 'the_naventi.jpg',
                     'theme_world_title' => 'The Naventi',
                     'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, laudantium voluptatum perferendis assumenda totam amet, praesentium qui nihil non aperiam excepturi error maxime ab dolore, accusantium eveniet explicabo nostrum eaque?'
                 ),
                 array(
-                    'img_name' => '',
-                    'img_theme_world' => '',
+                    'img_name' => 'the_political_system.jpg',
+                    'img_theme_world' => 'the_political_system.jpg',
                     'theme_world_title' => 'The Political System',
                     'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, laudantium voluptatum perferendis assumenda totam amet, praesentium qui nihil non aperiam excepturi error maxime ab dolore, accusantium eveniet explicabo nostrum eaque?'
                 ),
                 array(
-                    'img_name' => '',
-                    'img_theme_world' => '',
+                    'img_name' => 'the_prison_for_immortals.jpg',
+                    'img_theme_world' => 'the_prison_for_immortals.jpg',
                     'theme_world_title' => 'The Prison for Immortals',
                     'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, laudantium voluptatum perferendis assumenda totam amet, praesentium qui nihil non aperiam excepturi error maxime ab dolore, accusantium eveniet explicabo nostrum eaque?'
                 ),
                 array(
-                    'img_name' => '',
-                    'img_theme_world' => '',
+                    'img_name' => 'the_red_mirrors.jpg',
+                    'img_theme_world' => 'the_red_mirrors.jpg',
                     'theme_world_title' => 'The Red Mirrors',
                     'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, laudantium voluptatum perferendis assumenda totam amet, praesentium qui nihil non aperiam excepturi error maxime ab dolore, accusantium eveniet explicabo nostrum eaque?'
                 ),
                 array(
-                    'img_name' => '',
-                    'img_theme_world' => '',
+                    'img_name' => 'the_tanabras_world.jpg',
+                    'img_theme_world' => 'the_tanabras_world.jpg',
                     'theme_world_title' => "The Tanabra's World",
                     'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, laudantium voluptatum perferendis assumenda totam amet, praesentium qui nihil non aperiam excepturi error maxime ab dolore, accusantium eveniet explicabo nostrum eaque?'
                 ),
